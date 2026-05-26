5596e444c97ba92cfbec06c09983ea3b3c15fc45
rakibhossain2025 <rakibhossain4230@gmail.com>
2026-05-26 21:04:16 +0600

    update

---
+++ mod-1/dist/primitive.js
@@ -0,0 +1,10 @@
+"use strict";
+//JS - TS
+//JS: string,boolean,number,null,undefined
+//TS: never,unknown,void,any
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = 'rak';
+// userName =123 //! type 'number' is not assignable to type 'string'
+userName = 'rakibul';
+const userEmail = 'rakibul@bmail.com';
+//# sourceMappingURL=primitive.js.map
